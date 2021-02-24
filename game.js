67f97d55dfa4cd9075e9eb1f59516a2be743fa71
Aja Strommen <a.g.mechanix@gmail.com>
2021-02-23 23:20:18 -0500

    pseudocode of game play

--- game.js
+++ game.js
@@ -1,5 +1,31 @@
 function playGame() {
   console.log("welcome!")
+  prompt("How many rounds to play?")
 }
 
-playGame();
+// To do
+// begin condition
+//  take user input for rounds
+
+// playing condition
+//  generate computer selection
+//  take user selection
+//    case insensitive
+//    have default value of rock
+//  compare answers for winner
+
+// win condition
+//  scissor beats paper
+//  paper beats rock
+//  rock beats scissor
+//  show winner
+//  show current total score
+
+// end condition
+//  show final score
+
+
+// extra
+// validate user inputs
+// taunt user if loser
+// cheer user if winner
